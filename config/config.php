d0a8e32a29f124a78a596f6d630a909655c249c7
Ashley Hau <ashhau@gmail.com>
2022-01-29 01:07:17 -0800

    config

--- config/config.php
+++ config/config.php
@@ -3,9 +3,15 @@
 session_start(); // most pages need sessions
 // and they need the db credentials
 
-define('DB_HOST', '303.itpwebdev.com');
-define('DB_USER', 'haua_db_user');
-define('DB_PASS', 'itp303student');
-define('DB_NAME', 'haua_fairsplit');
+define('DB_HOST', 'remotemysql.com');
+define('DB_USER', 'ubvVRau563');
+define('DB_PASS', 'Xauy6A4W85');
+define('DB_NAME', 'ubvVRau563');
+
+// OLD
+// DB_HOST: 303.itpwebdev.com
+// DB_USER: haua_db_user
+// DB_PASS: itp303student
+// DB_NAME: haua_fairsplit
 
 ?>
