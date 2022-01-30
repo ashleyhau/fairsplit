153eb32d1c61daae1cd547df6de7052b18775477
Ashley Hau <ashhau@gmail.com>
2022-01-29 21:57:49 -0800

    change database host

--- config/config.php
+++ config/config.php
@@ -3,15 +3,21 @@
 session_start(); // most pages need sessions
 // and they need the db credentials
 
-define('DB_HOST', 'remotemysql.com');
-define('DB_USER', 'ubvVRau563');
-define('DB_PASS', 'Xauy6A4W85');
-define('DB_NAME', 'ubvVRau563');
+define('DB_HOST', 'sql3.freemysqlhosting.net');
+define('DB_USER', 'sql3469123');
+define('DB_PASS', 'Kp6da95e9b');
+define('DB_NAME', 'sql3469123');
 
 // OLD
 // DB_HOST: 303.itpwebdev.com
 // DB_USER: haua_db_user
 // DB_PASS: itp303student
 // DB_NAME: haua_fairsplit
+
+// remotemysql.com
+// define('DB_HOST', 'remotemysql.com');
+// define('DB_USER', 'ubvVRau563');
+// define('DB_PASS', 'Xauy6A4W85');
+// define('DB_NAME', 'ubvVRau563');
 
 ?>
